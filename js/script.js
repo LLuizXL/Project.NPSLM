5dd376b4cf8a239ce8ab306c165dbeb5eacb2fcd
LuizG <luiz.mwlo76@gmail.com>
2025-03-03 19:06:32 -0300

    adding audio for homepage

--- js/script.js
+++ js/script.js
@@ -7,4 +7,20 @@ $(document).ready(function () {
         $('#audio-start').get(0).play();
 
     }
+
+
+    var audioStart = $('#audio-start').get(0);
+
+    function playAudioStart() {
+        audioStart.play();
+    }
+    
+    function pauseAudioStart() {
+        audioStart.pause();
+    }
+    
+
+
 });
+
+
